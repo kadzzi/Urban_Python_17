29ac350b4a3e8f132a7022c1c8cd6bb46566c418
Dmitry <kadzzi@gmail.com>
2024-11-07 01:27:55 +0300

    Routing Tasks, updated routing Users

--- app/routers/user.py
+++ app/routers/user.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import Session
 from backend.db_depends import get_db
 # Аннотации, Модели БД и Pydantic.
 from typing import Annotated
-from models import User
+from models import User, Task
 from schemas import CreateUser, UpdateUser
 # Функции работы с записями.
 from sqlalchemy import insert, select, update, delete
@@ -78,7 +78,29 @@ async def delete_user(db: Annotated[Session, Depends(get_db)], user_id: int):
 
     db.execute(delete(User).where(User.id == user_id))
     db.commit()
+    tasks_ids = [x.id for x in db.scalars(select(Task).where(Task.user_id == user_id)).all()]
+    if len(tasks_ids) > 0:
+        for id_ in tasks_ids:
+            db.execute(delete(Task).where(Task.id == id_))
+            db.commit()
     return {
         'status_code': status.HTTP_200_OK,
         'transaction': 'User delete is successful!'
     }
+
+
+@user_router.get('/user_id/tasks')
+async def tasks_by_user_id(db: Annotated[Session, Depends(get_db)], user_id: int):
+    user = db.scalar(select(User).where(User.id == user_id))
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='User was not found'
+        )
+    tasks = db.scalars(select(Task).where(Task.user_id == user_id)).all()
+    if len(tasks) == 0:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='Tasks were not found'
+        )
+    return tasks
